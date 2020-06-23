070e0521fdb886a0f9fb97b10095677cad0fde4b
Hassenne <attig.benyoussefhassenne@gmail.com>
2020-06-23 11:26:15 +0200

    First step_Rest API with NestJS

---
+++ quotes-server/src/quotes/quotes.controller.ts
@@ -0,0 +1,44 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  Put,
+  Delete,
+} from '@nestjs/common';
+import { QuotesService } from './quotes.service';
+import { CreateQuoteDto } from './dto/create-quote.dto';
+import { Quote } from './interfaces/quote.interface';
+import { ApiTags, ApiParam } from '@nestjs/swagger';
+
+@ApiTags('quotes')
+@Controller('quotes')
+export class QuotesController {
+  constructor(private quotesService: QuotesService) {}
+  @Get() // localhost:3000/quotes
+  getQuotes(): Quote[] {
+    return this.quotesService.getQuotes();
+  }
+  @ApiParam({ name: 'id' })
+  @Get(':id')
+  getQuote(@Param('id') id): Quote {
+    return this.quotesService.getQuote(id);
+  }
+  @Post()
+  CreateQuote(@Body() createQuoteDto: CreateQuoteDto): Quote {
+    return this.quotesService.createQuote(createQuoteDto);
+  }
+
+  @ApiParam({ name: 'id' })
+  @Put(':id')
+  updateQuote(@Param('id') id, @Body() updateQuoteDto: CreateQuoteDto): Quote {
+    return this.quotesService.updateQuote(id, updateQuoteDto);
+  }
+
+  @ApiParam({ name: 'id' })
+  @Delete(':id')
+  deleteQuote(@Param('id') id): Quote {
+    return this.quotesService.deleteQuote(id);
+  }
+}
