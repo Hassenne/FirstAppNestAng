2dde405d5e9025096e30413444b4512386afe1d0
Hassenne <attig.benyoussefhassenne@gmail.com>
2020-08-25 17:21:39 +0200

    Adding Lazy Loading module and random avatar background

--- quotes-client/src/app/app.module.ts
+++ quotes-client/src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { MaterialModule } from "./material.module";
-import { HomeComponent } from './home/home.component';
+import { HomeComponent } from "./home/home.component";
 
 @NgModule({
   declarations: [AppComponent, HomeComponent],
