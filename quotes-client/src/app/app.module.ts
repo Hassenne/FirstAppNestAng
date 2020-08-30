03274c553c859f2ce72b4046b93de7c3e24a0d4f
Hassenne <attig.benyoussefhassenne@gmail.com>
2020-08-30 16:49:14 +0200

    Connecting to NestJS API and Finishing CRUD

--- quotes-client/src/app/app.module.ts
+++ quotes-client/src/app/app.module.ts
@@ -6,6 +6,8 @@ import { AppComponent } from "./app.component";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { MaterialModule } from "./material.module";
 import { HomeComponent } from "./home/home.component";
+import { HttpClientModule } from "@angular/common/http";
+import { FormsModule } from "@angular/forms";
 
 @NgModule({
   declarations: [AppComponent, HomeComponent],
@@ -14,6 +16,8 @@ import { HomeComponent } from "./home/home.component";
     AppRoutingModule,
     BrowserAnimationsModule,
     MaterialModule,
+    HttpClientModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
